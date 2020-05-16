f1894b61ffc0e3820f224ae34f4a43ce5ffed22b
Camiel Doorenweerd <c.doorenweerd@gmail.com>
2020-05-16 10:10:03 -1000

    tested and works

--- chao2.py
+++ chao2.py
@@ -7,16 +7,18 @@ import pandas as pd
 
 
 listoffastas = os.listdir('./species_fastas/')
+print("Calculating Chao2 for " + str(len(listoffastas)) + " species")
 outputfile = 'chao2_estimates.csv'
 
 
 speciesstats = []
 for fasta in listoffastas:
     speciesname = os.path.splitext(fasta)[0]
-    cmd = str("Rscript --vanilla SpideR_chao2.R -f ./species_fastas/") + fasta # this works from command line but not in subprocess command
-    chao2est = subprocess.check_output(cmd, shell=True, universal_newlines=True)
+    print("Running " + str(speciesname))
+    cmd = str("Rscript --vanilla SpideR_chao2.R -f ./species_fastas/") + fasta
+    chao2result = subprocess.check_output(cmd, shell=True, universal_newlines=True)
     speciesstats.append({'species': speciesname,
-                         'chao2': chao2est})
+                         'chao2result':chao2result})
 
 totaldf = pd.DataFrame(speciesstats)
 totaldf.to_csv(outputfile, index=False)
